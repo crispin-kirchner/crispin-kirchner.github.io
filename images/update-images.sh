c914a1d336f27d6300d557f6661ba9681839459e
Crispin Kirchner <kcrispin@web.de>
2017-07-12 17:55:01 +0300

    added autorotation for fullsize images

--- images/update-images.sh
+++ images/update-images.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-mkdir -p preview
+mkdir -p preview full
 
 for i in orig/*; do
   base=$(basename "$i")
   if [ ! -e "$base" ] ; then
     convert "$i" -auto-orient -scale 800 "$base"
     convert "$i" -auto-orient -scale 200 "preview/$base"
+  fi
+  
+  if [ ! -e "preview/$base" ] ; then
+    convert "$i" -auto-orient -scale 200 "preview/$base"
+  fi
+  
+  if [ ! -e "full/$base" ] ; then
+    cp "$i" "full/$base" && jhead -autorot "full/$base" 
   fi
 done
