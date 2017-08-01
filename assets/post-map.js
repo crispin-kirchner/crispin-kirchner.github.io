6cf4ff01e56a8d5e56aebac49f001d6bce349ad5
Crispin Kirchner <kcrispin@web.de>
2017-08-01 14:34:14 +0300

    moved scripts to own files to clean up layout files

---
+++ assets/post-map.js
@@ -0,0 +1,28 @@
+var	YEAR = 0,
+    MONTH = 1,
+    DAY = 2;
+    
+function getMonthName(monthNumber) {
+  var monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  
+  return monthName[monthNumber - 1];
+}
+
+function getDateString(date) {
+  return getMonthName(date[MONTH]) + " " + date[DAY] + ", " + date[YEAR];
+}
+
+var map = L.map('map');
+
+$.getJSON('/gelberzwerg/recorded-trajectory/statistics.json', function(statistics) {
+  map.fitBounds(L.latLngBounds(statistics.topLeft, statistics.bottomRight));
+  $("#totalDistance").text("Total distance cycled: " + statistics.totalDistanceKm.toFixed(0) + " km (" + getDateString(statistics.lastUpdated) + ")");
+});
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors '
+}).addTo(map);
+
+$.getJSON('/gelberzwerg/recorded-trajectory/trajectory.json', function(track) {
+  L.geoJson(track, { style: {color: "#f38148"}}).addTo(map);
+});
