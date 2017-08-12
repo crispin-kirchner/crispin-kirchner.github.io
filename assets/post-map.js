d72a63b290b68b783ab2d95907431bb62f8ff2c7
Crispin Kirchner <kcrispin@web.de>
2017-08-12 14:10:04 +0300

    implemented dynamically loading different means & key on ui

--- assets/post-map.js
+++ assets/post-map.js
@@ -12,6 +12,14 @@ function getDateString(date) {
   return getMonthName(date[MONTH]) + " " + date[DAY] + ", " + date[YEAR];
 }
 
+function getKeySvg(color) {
+  var width = 25;
+  
+  var svg = '<svg width="' + width + '" height="4"><line x1="0" y1="1" x2="' + width +
+            '" y2="1" style="stroke:' + color + ';stroke-width:4" /></svg>';
+  return svg;
+}
+
 var map = L.map('map');
 
 $.getJSON('/recorded-trajectory/means/bicycle/statistics.json', function(statistics) {
@@ -23,6 +31,12 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors '
 }).addTo(map);
 
-$.getJSON('/recorded-trajectory/means/bicycle/downsampled-trajectory.json', function(track) {
-  L.geoJson(track, { style: {color: "#f38148"}}).addTo(map);
+$.getJSON('recorded-trajectory/means/means.json', function(allMeans) {
+  allMeans.forEach(function(means) {
+    $.getJSON('recorded-trajectory/means/' + means.name + '/downsampled-trajectory.json', function(track) {
+      L.geoJson(track, {style: {color: means.color}}).addTo(map);
+    });
+    
+    $("#mapKey").append("<div><p>" + getKeySvg(means.color) + ' ' + means.name + "</p></div>");
+  });
 });
