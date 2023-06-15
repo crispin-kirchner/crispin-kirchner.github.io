fde9ee700033d0be384dc1f3d01cd190a4fb505c
Crispin Kirchner <6740795+crispin-kirchner@users.noreply.github.com>
2023-06-15 19:33:32 +0200

    add first post

--- entries.js
+++ entries.js
@@ -1,2 +1,9 @@
 ENTRIES = {
+  "2023-06-15T12:07:00-kranj": {
+    img: "DSC03109.JPG",
+    title: "Kranj, die Hauptstadt der slowenischen Alpen",
+    latLon: [46.23841, 14.35561],
+    date: "2023-06-15",
+    orientation: "landscape"
+	}
 };
--- images/preprocess-images.sh
+++ images/preprocess-images.sh
@@ -5,12 +5,12 @@
 # TODO Bildgrössen so abspeichern dass ich sie aus JS und in SH-Script einlesen kann
 # TODO progressives encoding
 
-for i in *.JPG ; do
-  convert "$i" -scale 360 "bg/$i"
-  convert "$i" -scale 64x64^ -gravity center -crop 64x64+0+0 "thumbs/$i"
-  convert "$i" -scale 720 "720w/$i"
-  convert "$i" -scale 1280 "1280w/$i"
-  convert "$i" -scale 1920 "1920w/$i"
-  convert "$i" -scale 2560 "2560w/$i"
-  convert "$i" -scale 4096 "4096w/$i"
+for i in orig/*.JPG ; do
+  convert "$i" -scale 360 "bg/${i:5}"
+  convert "$i" -scale 64x64^ -gravity center -crop 64x64+0+0 "thumbs/${i:5}"
+  convert "$i" -scale 720 "720w/${i:5}"
+  convert "$i" -scale 1280 "1280w/${i:5}"
+  convert "$i" -scale 1920 "1920w/${i:5}"
+  convert "$i" -scale 2560 "2560w/${i:5}"
+  convert "$i" -scale 4096 "4096w/${i:5}"
 done
