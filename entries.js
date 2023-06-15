60d8b919f1dc10d7253fd73112de9cb52ffadf78
Crispin Kirchner <6740795+crispin-kirchner@users.noreply.github.com>
2023-06-15 23:01:00 +0200

    add two more pictures

--- entries.js
+++ entries.js
@@ -2,8 +2,25 @@ ENTRIES = {
   "2023-06-15T12:07:00-kranj": {
     img: "DSC03109.JPG",
     title: "Kranj, die Hauptstadt der slowenischen Alpen",
+    text: "Die Stadt hat mich sehr an Bern erinnert. Sie ist, obwohl sie relativ klein ist, die viertgrößte Stadt Sloweniens. Die Altstadt ist an drei Seiten von Gewässern umgeben, an einer Seite sogar wie Bern mit einem tiefen Graben, der einen wunderbar wild-verwunschenen Park enthält. Beim Einkaufen im Bioladen bekam ich nicht nur ein selbst gemachtes Hafer-Leckerli geschenkt, sondern auch Tipps, was ich mir auf meinem Weg unbedingt noch anschauen muss und wo ich den besten Wetterbericht bekomme.<br/>Die Altstadt ist mit einem 1.3km langen Tunnelsystem unterbuddelt, das im 2. Weltkrieg als Luftschutzbunker diente und jetzt ein Museum ist. Leider war es nicht geöffnet.",
     latLon: [46.23841, 14.35561],
     date: "2023-06-15",
     orientation: "landscape"
-	}
+  },
+  "2023-06-15T15:15:00-wildwandern": {
+    img: "DSC03121.JPG",
+    title: "Auf diesem Bild hat sich ein Wanderweg versteckt. Findest du ihn?",
+    text: "Der Zustieg zur ersten Hütte ist wunderbar wild. Der Zecken-Check am Abend zum Glück trotzdem negativ.",
+    latLon: [46.09511, 14.08366],
+    date: "2023-06-15",
+    orientation: "portrait"
+  },
+  "2023-06-15T16:13:00-alpweide": {
+    img: "DSC03125.JPG",
+    title: "Eine prächtige Alpweide auf dem Weg nach oben",
+    text: "Es summt und schwirrt um die Beine; Moose, tausend bunte Blüten, Moose, und bei jedem Schritt duftet es nach wildem Thymian.",
+    latLon: [46.09409, 14.07299],
+    date: "2023-06-15",
+    orientation: "landscape"
+  },
 };
--- images/preprocess-images.sh
+++ images/preprocess-images.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
 
-# TODO nur Bilder converten die es noch nicht gibt
 # TODO Ordner anlegen wenn es sie noch nicht gibt
-# TODO Bildgrössen so abspeichern dass ich sie aus JS und in SH-Script einlesen kann
 # TODO progressives encoding
 
-for i in orig/*.JPG ; do
-  convert "$i" -scale 360 "bg/${i:5}"
-  convert "$i" -scale 64x64^ -gravity center -crop 64x64+0+0 "thumbs/${i:5}"
-  convert "$i" -scale 720 "720w/${i:5}"
-  convert "$i" -scale 1280 "1280w/${i:5}"
-  convert "$i" -scale 1920 "1920w/${i:5}"
-  convert "$i" -scale 2560 "2560w/${i:5}"
-  convert "$i" -scale 4096 "4096w/${i:5}"
+function doConvert {
+  local targetFolder="$3"
+  local args="$2"
+  local sourceFilePath="$1"
+  local sourceFilename="$(basename "$sourceFilePath")"
+  local target="$targetFolder/$sourceFilename"
+  if [ -e "$target" ]; then
+    return 0
+  fi
+  echo "$target"
+  convert "$sourceFilePath" $args -interlace line "$target"
+}
+
+mkdir -p bg
+mkdir -p thumbs
+mkdir -p 720w
+mkdir -p 1280w
+mkdir -p 1920w
+mkdir -p 2560w
+mkdir -p 4096w
+
+for i in ~/storage/shared/Imaging\ Edge\ Mobile/blog/*.JPG ; do
+  doConvert "$i" "-scale 360" "bg"
+  doConvert "$i" "-scale 64x64^ -gravity center -crop 64x64+0+0" "thumbs"
+  doConvert "$i" "-scale 720" "720w"
+  doConvert "$i" "-scale 1280" "1280w"
+  doConvert "$i" "-scale 1920" "1920w"
+  doConvert "$i" "-scale 2560" "2560w"
+  doConvert "$i" "-scale 4096" "4096w"
 done
+
