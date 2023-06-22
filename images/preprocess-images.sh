bf0aaea8753de474cad65bb5b8408a3f82e34e51
Crispin Kirchner <6740795+crispin-kirchner@users.noreply.github.com>
2023-06-22 22:30:04 +0200

    process images in single command

--- images/preprocess-images.sh
+++ images/preprocess-images.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-# TODO Ordner anlegen wenn es sie noch nicht gibt
-# TODO progressives encoding
-
-function doConvert {
-  local targetFolder="$3"
-  local args="$2"
-  local sourceFilePath="$1"
-  local sourceFilename="$(basename "$sourceFilePath")"
-  local target="$targetFolder/$sourceFilename"
-  if [ -e "$target" ]; then
-    return 0
-  fi
-  echo "$target"
-  convert "$sourceFilePath" $args -interlace line -quality 92 "$target"
+function imInstruction {
+  echo "( mpr:XY $1 -interlace line -quality 92 +write $2 )"
 }
 
 mkdir -p bg
@@ -25,12 +13,19 @@ mkdir -p 2560w
 mkdir -p 4096w
 
 for i in ~/storage/shared/Imaging\ Edge\ Mobile/blog/*.JPG ; do
-  doConvert "$i" "-scale 360" "bg"
-  doConvert "$i" "-scale 64x64^ -gravity center -crop 64x64+0+0" "thumbs"
-  doConvert "$i" "-scale 720" "720w"
-  doConvert "$i" "-scale 1280" "1280w"
-  doConvert "$i" "-scale 1920" "1920w"
-  doConvert "$i" "-scale 2560" "2560w"
-  doConvert "$i" "-scale 4096" "4096w"
+  sourceFilename="$(basename "$i")"
+  if [ -e "4096w/$sourceFilename" ] ; then
+    continue
+  fi
+  command=""
+  command="$command $(imInstruction "-scale 360" bg/$sourceFilename)"
+  command="$command $(imInstruction "-scale 64x64^ -gravity center -crop 64x64+0+0" thumbs/$sourceFilename)"
+  command="$command $(imInstruction "-scale 720" 720w/$sourceFilename)"
+  command="$command $(imInstruction "-scale 1280" 1280w/$sourceFilename)"
+  command="$command $(imInstruction "-scale 1920" 1920w/$sourceFilename)"
+  command="$command $(imInstruction "-scale 2560" 2560w/$sourceFilename)"
+  command="$command $(imInstruction "-scale 4096" 4096w/$sourceFilename)"
+  echo $sourceFilename
+  convert "$i" -write mpr:XY +delete -respect-parentheses $command null:
 done
 
